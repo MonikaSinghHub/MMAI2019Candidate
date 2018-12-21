fe2d640f5c3f39405d234d0136aef36460fcd106
MonikaSinghHub <46040611+MonikaSinghHub@users.noreply.github.com>
2018-12-20 23:54:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from user_info import enter_data, read_from_db
+
+enter_data("112","Monika", "Singh", "60", "F", "Canadian", "NA")
+#read_from_db(101)
---
+++ user_info.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+def create_table():
+    con = mysql.connector.connect(user='root', password='monika', host='localhost', port='3306', database='user_db')
+    cursor = con.cursor()
+    cursor.execute(
+        "CREATE TABLE user_info(customer_id INTEGER, first_name VARCHAR, last_name VARCHAR, age INTEGER, sex VARCHAR, race VARCHAR, observation VARCHAR)")
+    con.commit()
+    con.close()
+
+def enter_data(custId,firstName,lastName,age,sex,race,observation):
+    con = mysql.connector.connect(user='root', password='monika', host='localhost', port='3306', database='user_db')
+    cursor = con.cursor()
+
+    try:
+        select_sql = "select * from user_info where customer_id = %s and first_name = %s and last_name = %s"
+        cursor.execute(select_sql, (custId, firstName, lastName))
+        data = cursor.fetchall()
+        if not data:
+            cursor.execute("insert into user_info (customer_id, first_name, last_name, age, sex, race, observation) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    (custId, firstName, lastName, age, sex, race, observation))
+            print('User got added to the database::' + firstName + ' ' + lastName)
+        else:
+            print('Customer already exists')
+    except Exception as e:
+        con.rollback()
+        print(e)
+    con.commit()
+    con.close()
+
+def read_from_db(userId):
+    con = mysql.connector.connect(user='root', password='monika', host='localhost', port='3306', database='user_db')
+    cursor = con.cursor()
+    cursor.execute("select * from user_info where customer_id= %s", (userId,))
+    data = cursor.fetchall()
+    if not data:
+        print("No data")
+    else:
+        for row in data:
+            print(row)
+    con.close()
